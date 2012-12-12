1f5fa11a1c1038322cf15bf7be1dc6183524a896
Val <niothiel@gmail.com>
2012-12-11 19:45:59 -0500

    Changed requests to EveMarketData to batch

--- request.py
+++ request.py
@@ -14,11 +14,16 @@ regions = {
 	'heimatar': 30002510
 }
 
-evecMarketstat = 'http://api.eve-central.com/api/marketstat?'
-evemPriceHistory = 'http://api.eve-marketdata.com/api/item_history2.json?char_name=asdf&region_ids=10000002&days=5&'
-#statStr = 'http://api.eve-central.com/api/marketstat?typeid=%d&regionlimit=10000030'
+evecMarketstat =	'http://api.eve-central.com/api/marketstat?'
+evemPriceHistory =	'http://api.eve-marketdata.com/api/item_history2.json?char_name=asdf&days=5&'
 
 typeToName = {}
+
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
 
 def formatNum(price):
 	if price < 1000:
@@ -30,6 +35,7 @@ def formatNum(price):
 
 class MarketItem:
 	def __init__(self):
+		self.typeId = None
 		self.name = None
 		self.buyOrders = 0
 		self.buyPrice = 0
@@ -54,6 +60,57 @@ class MarketItem:
 
 	def __str__(self):
 		return self.__repr__()
+		
+	def fromEveMarketData(self, orderDict):
+		# Getting the stuff from eve-marketdata.com. Effin messy.
+		orderDict = orderDict['emd']['result']
+		
+		# Again, bit awkward, adding the total order history
+		self.soldOrderHistory = []
+		sumOrders = 0
+		numDataPoints = 0
+		for entry in orderDict:
+			if entry['row']['typeID'] == str(self.typeId):
+				numOrders = int(entry['row']['orders'])
+				sumOrders += numOrders
+				numDataPoints += 1
+		
+		if numDataPoints <> 0:
+			self.soldOrders = float(sumOrders) / numDataPoints
+		
+	def fromEveMarketDataPerItem(self, orderDict):
+		if self.typeId == None:
+			return
+	
+		url = evemPriceHistory
+		url += 'region_ids=%d&type_ids=%d' % (regions['the_forge'], self.typeId)
+		print url
+		
+		data = urllib2.urlopen(url).read()
+		orderDict = json.loads(data)
+		
+		self.fromEveMarketData2(orderDict)
+	
+	def fromEveCentral(self, itemNode):
+		self.typeId = int(itemNode.attrib['id'])
+		self.name = getItemName(self.typeId)
+		
+		if self.name == None:
+			return
+
+		buyNode = itemNode.find('buy')
+		sellNode = itemNode.find('sell')
+		allNode = itemNode.find('all')
+
+		self.buyOrders = int(buyNode.find('volume').text)
+		self.buyPrice = float(buyNode.find('max').text)
+		self.sellOrders = int(sellNode.find('volume').text)
+		self.sellPrice = float(sellNode.find('min').text)
+		self.totalOrders = int(allNode.find('volume').text)
+
+		if self.buyOrders <> 0 and self.buyPrice <> 0 and self.sellOrders <> 0 and self.sellPrice <> 0:
+			self.priceDifference = abs(self.buyPrice - self.sellPrice) / self.buyPrice * 100
+			self.volumeDifference = abs(self.buyOrders - self.sellOrders) / float(self.totalOrders) * 100
 
 def initTypeToNameDB():
 	f = open('typeid.txt', 'r')
@@ -70,85 +127,52 @@ def getItemName(typeId):
 	else:
 		return None
 
-def getItems(typeIds, system=None, region=None):
+def _getItems(typeIds, region):
 	if len(typeIds) == 0:
 		return []
 
-	if system == None and region == None:			# If nothing's specified, just return
-		return []
-
 	# Generate the url to use for the query
-	url = evecMarketstat
-	if system <> None:					# Set the system if we have one
-		url += 'usesystem=%d&' % systems[system]
+	urlCentral = evecMarketstat
+	urlData = evemPriceHistory
+	
 	if region <> None:					# Set the region if we have one
-		url += 'regionlimit=%d&' % regions[region]
-	for typeId in typeIds:					# Add all the typeids
-		if getItemName(typeId) <> None:			# Check to make sure the thing actually exists.
-			url += 'typeid=%d&' % typeId
-	url = url[:-1]						# Strip the ending &
+		urlCentral += 'regionlimit=%d&' % regions[region]
+		urlData += 'region_ids=%d&' % regions[region]
+	
+	urlData += 'type_ids='
+	
+	for typeId in typeIds:				# Add all the typeids
+		if getItemName(typeId) <> None:	# Check to make sure the thing actually exists.
+			urlCentral += 'typeid=%d&' % typeId
+			urlData += '%d,' % typeId
+			
+	urlCentral = urlCentral[:-1]		# Strip the ending &
+	urlData = urlData[:-1]
 
-	print 'Using URL:', url
+	print 'Using URL:', urlCentral
+	print 'Using URL:', urlData
 
-	htmlReply = urllib2.urlopen(url).read()
+	htmlReply = urllib2.urlopen(urlCentral).read()
 	root = ET.fromstring(htmlReply)[0]
 	
+	eveMarketDataReply = urllib2.urlopen(urlData).read()
+	eveMarketDataDict = json.loads(eveMarketDataReply)
+	
 	items = []
-	for typeNodes in root:
-		item = getItemFromNode(typeNodes)
-		if item <> None:
-			items.append(item)
+	for typeNode in root:
+		item = MarketItem()
+		item.fromEveCentral(typeNode)
+		item.fromEveMarketData(eveMarketDataDict)
+		items.append(item)
 
 	return items
-
-def getItemFromNode(itemNode):
-	typeId = int(itemNode.attrib['id'])
-
-	if getItemName(typeId) == None:
-		return
+	
+def getItems(typeIds, region):
+	totalItems = []
+	for chunk in chunks(typeIds, 1000):
+		totalItems += _getItems(chunk, region)
 		
-	# Getting the stuff from eve-marketdata.com. Effin messy.
-	# Generate the url to use for the query
-	url = evemPriceHistory + 'type_ids=%d' % typeId
-	print 'Using URL:', url
-	htmlReply = urllib2.urlopen(url).read()
-	priceHistory = json.loads(htmlReply)
-	priceHistory = priceHistory['emd']['result']
-	#pprint( priceHistory )
-	#exit(1)
-
-	buyNode = itemNode.find('buy')
-	sellNode = itemNode.find('sell')
-	allNode = itemNode.find('all')
-
-	m = MarketItem()
-	m.name = getItemName(typeId)
-	m.buyOrders = int(buyNode.find('volume').text)
-	m.buyPrice = float(buyNode.find('max').text)
-	m.sellOrders = int(sellNode.find('volume').text)
-	m.sellPrice = float(sellNode.find('min').text)
-	m.totalOrders = int(allNode.find('volume').text)
-	
-	# Again, bit awkward, adding the total order history
-	m.soldOrderHistory = []
-	sumOrders = 0
-	numDataPoints = len(priceHistory)
-	for entry in priceHistory:
-		numOrders = int(entry['row']['orders'])
-		#m.soldOrderHistory.append(entry['row'])
-		sumOrders += numOrders
-	
-	if numDataPoints <> 0:
-		m.soldOrders = float(sumOrders) / numDataPoints
-
-	if m.buyOrders == 0 or m.buyPrice == 0 or m.sellOrders == 0 or m.sellPrice == 0:
-		m.priceDifference = 0
-		m.volumeDifference = 0
-	else:
-		m.priceDifference = abs(m.buyPrice - m.sellPrice) / m.buyPrice * 100
-		m.volumeDifference = abs(m.buyOrders - m.sellOrders) / float(m.totalOrders) * 100
-
-	return m
+	return totalItems
 
 def itemCriteria(item):
 	if item.priceDifference < 15:
@@ -175,8 +199,7 @@ def main():
 	initTypeToNameDB()
 
 	typeIds = range(34, 41) + range(178, 202) + range(377, 551) + range(551, 1357) + range(16305, 16536)
-	itemList = getItems(typeIds, 'jita')
-
+	itemList = getItems(typeIds, 'the_forge')
 	itemList = filter(itemCriteria, itemList)
 	itemList = sorted(itemList, key=sortCriteria)
 
